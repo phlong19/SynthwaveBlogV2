508e9ec1af70d33cb09aeed28f41938163289388
phlong19 <phl19.dev@gmail.com>
2024-07-21 23:49:40 +0700

    chore: added app model

---
+++ src/constants/kcal.ts
@@ -0,0 +1,29 @@
+// total daily energy expenditure
+export const TDEE = [
+  {
+    name: "sedentary",
+    value: 1.2,
+    description: "little or no exercise",
+  },
+  {
+    name: "lightly",
+    value: 1.375,
+    description: "light exercise or sports 1-3 days a week",
+  },
+  {
+    name: "moderately",
+    value: 1.55,
+    description: "moderate exercise or sports 3-5 days a week",
+  },
+  {
+    name: "very",
+    value: 1.725,
+    description: "hard exercise or sports 6-7 days a week",
+  },
+  {
+    name: "extra",
+    value: 1.9,
+    description:
+      "very hard exercise or sports phycical job or training twice a day",
+  },
+];
---
+++ src/model/model.ts
@@ -0,0 +1,84 @@
+interface BasicInformation {
+  id: string;
+  name: string;
+  displayName: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isDeleted: boolean;
+}
+
+interface User extends BasicInformation {
+  email: string;
+  phone: string;
+  height: number; // cm
+  weight: number; // kg
+  birth: Date; // cal age
+  gender: boolean;
+  workout: string; // TDEE
+  avatar: string;
+}
+
+interface Category extends BasicInformation {
+  hasSub: boolean;
+  sub: string[];
+  slug: string;
+  description: string;
+  status: boolean;
+}
+
+enum PostType {
+  vehicle = "VEHICLE",
+  blog = "BLOG",
+}
+
+interface Post {
+  id: string;
+  title: string;
+  name: string; // camel case
+  slug: string; // slugify
+  like?: string[];
+  likeCount?: number;
+  view?: string[];
+  viewCount?: number;
+  author: string;
+  description: string;
+  content: string;
+  tags: string[]; // categories
+  type: PostType;
+  hashtags: string; // #food#hanoi => split & render
+  thumb: string;
+}
+
+interface Comment extends BasicInformation {
+  author: string;
+  content: string;
+  hasSub: boolean;
+  sub: string;
+}
+
+// cal kcal
+interface Daily {
+  id: string;
+  date: Date;
+  user: string;
+  total: number; // total kcal from the day
+}
+
+interface Meal {
+  id: string;
+  daily: string; // what date is it
+  time: Date; // what time is it?
+  image?: string;
+  kcal: number; // calories
+  ingredients: string[];
+}
+
+interface Ingredient extends BasicInformation {
+  version: string;
+  calcalories_per_100g: number;
+  protein_per_100g: number;
+  carbs_per_100g: number;
+  fat_per_100g: number;
+}
+
+export type { Ingredient, User, Category, Comment, Post, Daily, Meal };
